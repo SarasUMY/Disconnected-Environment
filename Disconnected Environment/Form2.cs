7f6152be8ee18d3acbd16a0c26c8d8d7a9300326
saraz <bagas.saras.ft21@mail.umy.ac.id>
2023-06-22 17:23:35 +0700

    Add project files.

---
+++ Disconnected Environment/Form2.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Disconnected_Environment
+{
+    public partial class Form2 : Form
+    {
+        private string connectionString = "Server=localhost;Database=blob;Uid=root;Pwd=BSBPfeb1234_;";
+        private MySqlConnection koneksi;
+
+        private void refreshform()
+        {
+            nmp.Text = "";
+            nmp.Enabled = false;
+            btnSave.Enabled = false;
+            btnClear.Enabled = false;
+        }
+        public Form2()
+        {
+            InitializeComponent();
+            koneksi = new MySqlConnection(connectionString);
+            refreshform();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView()
+        {
+            koneksi.Open();
+            string str = "select nama_prodi from pabd.prodi";
+            MySqlDataAdapter da = new MySqlDataAdapter(str, koneksi);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            koneksi.Close();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            dataGridView();
+            btnOpen.Enabled = false;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            nmp.Enabled = true;
+            btnSave.Enabled = true;
+            btnClear.Enabled = true;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            refreshform();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string nmProdi = nmp.Text;
+
+            if (nmProdi == "")
+            {
+                MessageBox.Show("Masukkan Nama Prodi", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                koneksi.Open();
+                string str = "Insert into pabd.prodi (nama_prodi)" + "values(@id)";
+                MySqlCommand cmd = new MySqlCommand(str, koneksi);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Add(new MySqlParameter("id", nmProdi));
+                cmd.ExecuteNonQuery();
+
+                koneksi.Close();
+                MessageBox.Show("Data Berhasil Disimpan", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dataGridView();
+                refreshform();
+            }
+
+        }
+
+        private void FormDataProdi_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Form1 hu = new Form1();
+            hu.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Disconnected Environment/Form3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Disconnected_Environment
+{
+    public partial class Form3 : Form
+    {
+        private string connectionString = "Server=localhost;Database=blob;Uid=root;Pwd=BSBPfeb1234_;";
+        private MySqlConnection koneksi;
+        private string nim, nama, alamat, jk, prodi;
+        private DateTime tgl;
+        BindingSource customerbindingSource = new BindingSource();
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            koneksi.Open();
+            MySqlDataAdapter dataAdapter = new MySqlDataAdapter(new MySqlCommand("Select m.nim, m.nama_mahasiswa, " + "m.alamat, m.jenis_kel, m.tgl_lahir, p.nama_prodi From pabd.mahasiswa m " + "join pabd.prodi p on m.id_prodi = p.id_prodi", koneksi));
+            DataSet ds = new DataSet();
+            dataAdapter.Fill(ds);
+
+            this.customerbindingSource.DataSource = ds.Tables[0];
+
+        }
+    }
+}
